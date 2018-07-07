af6d11db72b9cd422bfcc6d4b28daa89eefd428e
malek-boukhari <godsent615@gmail.com>
2018-07-07 14:15:43 +0200

    moved the inDeko page to its own activity

--- app/src/main/java/com/example/malek/tolleapp/MainActivity.java
+++ app/src/main/java/com/example/malek/tolleapp/MainActivity.java
@@ -14,35 +14,17 @@ import android.widget.CalendarView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-
 public class MainActivity extends AppCompatActivity {
-    private ViewPager mSlideViewPagerInDeko;
-    private LinearLayout mDotsLayoutIndeko;
-    private TextView[] mdots;
-    private SliderAdapterInDeko sliderAdapterInDeko;
-    private Button button, moreButton, calendarBtn;
+
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSlideViewPagerInDeko = (ViewPager) findViewById(R.id.slideViewPagerInDeko);
-        mDotsLayoutIndeko = (LinearLayout) findViewById(R.id.dotsLayoutInDeko);
-        sliderAdapterInDeko = new SliderAdapterInDeko(this);
-        mSlideViewPagerInDeko.setAdapter(sliderAdapterInDeko);
 
-        addDotsIndicator(0);
-        mSlideViewPagerInDeko.addOnPageChangeListener(viewListener);
-        moreButton = findViewById(R.id.more);
-        moreButton.setVisibility(View.INVISIBLE);
 
-        calendarBtn = findViewById(R.id.calendarButton);
-        calendarBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openCalendarActivity();
-            }
-        });
 
         button = findViewById(R.id.bestFitButton);
         button.setOnClickListener(new View.OnClickListener() {
@@ -51,79 +33,36 @@ public class MainActivity extends AppCompatActivity {
                 openMainBestFitActivity();
             }
         });
+
+        button = findViewById(R.id.inDekoButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainInDekoActivity();
+            }
+        });
     }
 
-    //this method is called by the onClick event of the button and calls the method openBrowserActivity
-    public void browser(View view) {
-        openBrowserActivity();
-    }
 
-    public void openCalendarActivity(){
-        Intent intent = new Intent(this, CalendarActivity.class);
-        startActivity(intent);
-    }
 
     //this method opens the activity BestFitActivity
-    public void openMainBestFitActivity(){
+    public void openMainBestFitActivity() {
 
-        Intent intent = new Intent(this,MainBestFitActivity.class);
+        Intent intent = new Intent(this, MainBestFitActivity.class);
         startActivity(intent);
     }
 
-    //This method opens the URL of Indeko in a browser
-    public void openBrowserActivity(){
-        String url = "http://147.172.96.25/atlas/";
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(url));
-        //This exception prevents the app from crashing if there is no browser installed
-        if (intent.resolveActivity(getPackageManager())!=null){
-            startActivity(intent);
-         }
+    //this method opens the activity MainInDekoActivity
+    public void openMainInDekoActivity(){
+        Intent intent = new Intent(this,MainInDekoActivity.class);
+        startActivity(intent);
     }
-
-    //This method is to generate the dots in the bottom of the slider and indicates the slider's position
-    public void addDotsIndicator(int position){
-        //mdots textview gets the length of the slider and then removes the views to avoid repetition
-        mdots = new TextView[sliderAdapterInDeko.getCount()];
-        mDotsLayoutIndeko.removeAllViews();
-
-        for (int i = 0; i < mdots.length; i++){
-            mdots[i] = new TextView(this);
-            mdots[i].setText(Html.fromHtml("&#8226;"));
-            mdots[i].setTextSize(35);
-            mdots[i].setTextColor(getResources().getColor(R.color.green));
-            mDotsLayoutIndeko.addView(mdots[i]);
-        }
-
-        if(mdots.length > 0){
-            mdots[position].setTextColor(getResources().getColor(R.color.DarkGray));
-        }
-    }
-
-    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
-
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-        }
-
-        @Override
-        public void onPageSelected(int i) {
-
-            addDotsIndicator(i);
-            //when the user reaches the last slide the button "mehr" becomes visible
-            if (i == mdots.length-1){
-
-                moreButton.setVisibility(View.VISIBLE);
-            }else {
-                moreButton.setVisibility(View.INVISIBLE);
-            }
-        }
-
-        @Override
-        public void onPageScrollStateChanged(int state) {
-
-        }
-    };
 }
+
+
+
+
+
+
+
 
---
+++ app/src/main/java/com/example/malek/tolleapp/MainInDekoActivity.java
@@ -0,0 +1,117 @@
+package com.example.malek.tolleapp;
+
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+
+public class MainInDekoActivity extends AppCompatActivity {
+    private ViewPager mSlideViewPagerInDeko;
+    private LinearLayout mDotsLayoutIndeko;
+    private TextView[] mdots;
+    private SliderAdapterInDeko sliderAdapterInDeko;
+    private Button button, moreButton, calendarBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_in_deko);
+        mSlideViewPagerInDeko = (ViewPager) findViewById(R.id.slideViewPagerInDeko);
+        mDotsLayoutIndeko = (LinearLayout) findViewById(R.id.dotsLayoutInDeko);
+        sliderAdapterInDeko = new SliderAdapterInDeko(this);
+        mSlideViewPagerInDeko.setAdapter(sliderAdapterInDeko);
+
+        addDotsIndicator(0);
+        mSlideViewPagerInDeko.addOnPageChangeListener(viewListener);
+        moreButton = findViewById(R.id.more);
+        //The button mehr is set to invisible by default
+        moreButton.setVisibility(View.INVISIBLE);
+
+        calendarBtn = findViewById(R.id.calendarButton);
+        calendarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCalendarActivity();
+            }
+        });
+
+    }
+
+    //this method is called by the onClick event of the button and calls the method openBrowserActivity
+    public void browser(View view) {
+        openBrowserActivity();
+    }
+
+    public void openCalendarActivity(){
+        Intent intent = new Intent(this, CalendarActivity.class);
+        startActivity(intent);
+    }
+
+
+    //This method opens the URL of Indeko in a browser
+    public void openBrowserActivity(){
+        String url = "http://147.172.96.25/atlas/";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        //This exception prevents the app from crashing if there is no browser installed
+        if (intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
+    }
+
+    //This method is to generate the dots in the bottom of the slider and indicates the slider's position
+    public void addDotsIndicator(int position){
+        //mdots textview gets the length of the slider and then removes the views to avoid repetition
+        mdots = new TextView[sliderAdapterInDeko.getCount()];
+        mDotsLayoutIndeko.removeAllViews();
+
+        for (int i = 0; i < mdots.length; i++){
+            mdots[i] = new TextView(this);
+            mdots[i].setText(Html.fromHtml("&#8226;"));
+            mdots[i].setTextSize(35);
+            mdots[i].setTextColor(getResources().getColor(R.color.green));
+            mDotsLayoutIndeko.addView(mdots[i]);
+        }
+
+        if(mdots.length > 0){
+            mdots[position].setTextColor(getResources().getColor(R.color.DarkGray));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+
+            addDotsIndicator(i);
+            //when the user reaches the last slide the button "mehr" becomes visible
+            if (i == mdots.length-1){
+
+                moreButton.setVisibility(View.VISIBLE);
+            }else {
+                moreButton.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+}
+
